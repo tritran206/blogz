feda7c6e75a764957ac2eb2e2296747c73a54c4a
Tri Tran <tritran20@gmail.com>
2017-06-27 22:26:11 -0700

    Added HTML templates

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/blog', methods=['POST', 'GET'])
+def show_blogs():
+    blogs = Blog.query.all()
+    return render_template('blogs.html', title="Your Blogs", blogs = blogs)
+
+@app.route('/newpost')
+def index():
+    return render_template('newpost.html', title="Add Posts")
+
+@app.route('/addpost', methods=['POST'])
+def add():
+    blog_title = request.form['blog']
+    blog_body = request.form['blog_body']
+    new_blog = Blog(blog_title, blog_body)
+    db.session.add(new_blog)
+    db.session.commit()
+
+    return redirect('/blog')
+
+
+if __name__ == '__main__':
+    app.run()
