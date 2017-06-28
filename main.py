3c4e407ca2014db64b860b806c8b55f87c2fc8e9
Tri Tran <tritran20@gmail.com>
2017-06-28 00:32:32 -0700

    Added validation and blog links

--- main.py
+++ main.py
@@ -18,10 +18,16 @@ class Blog(db.Model):
         self.body = body
 
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/blog')
 def show_blogs():
     blogs = Blog.query.all()
     return render_template('blogs.html', title="Your Blogs", blogs = blogs)
+
+
+def single_blog():
+    blog_id = int(request.args.get('id'))
+    blogs = Blog.query.get(blog_id)
+    return render_template('blogs.html', title="Current Blog", blogs = blogs)
 
 @app.route('/newpost')
 def index():
@@ -29,8 +35,22 @@ def index():
 
 @app.route('/addpost', methods=['POST'])
 def add():
+
+    title_error = ''
+    body_error = ''
+
     blog_title = request.form['blog']
     blog_body = request.form['blog_body']
+
+    if blog_title == "":
+        title_error = "Please enter a title for you blog"
+
+    if blog_body == "":
+        body_error = "Please enter content for your blog"
+
+    if body_error or title_error != '':
+        return render_template('newpost.html', title="Add Posts", title_error=title_error, body_error=body_error)
+
     new_blog = Blog(blog_title, blog_body)
     db.session.add(new_blog)
     db.session.commit()
