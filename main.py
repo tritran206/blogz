7acfe732c6273188d99d2744533c308f1d7b0769
Tri Tran <tritran20@gmail.com>
2017-07-15 14:19:00 -0700

    Added home page and hand handlers for index. You can now view each user's blogs

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Blog(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['signup', '/', 'allpost', 'login']
+    allowed_routes = ['signup', 'home', 'allpost', 'login', '/']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
@@ -143,54 +143,75 @@ def login():
 def logout():
     if 'username' in session:
         del session['username']
-    return redirect('/login')
+    return redirect('/blog')
+
+@app.route('/')
+def display_users():
+    users = User.query.all()
+    return render_template('index.html', title="Blog Users", users=users)
 
 @app.route('/blog')
 def home():
     if 'id' in request.args:
         return single_blog()
+    elif 'user' in request.args:
+        return users_blog()
     else:
         return show_blogs()
 
 
 def show_blogs():
     blogs = Blog.query.all()
+    users = User.query.all()
+    if 'username' in session:
+        return render_template('blogs.html', title="Your Blogs", blogs=blogs, users=users, username=session['username'])
+
     return render_template('blogs.html', title="Your Blogs", blogs = blogs)
+
+def users_blog():
+    users_id = int(request.args.get('user'))
+    user = User.query.get(users_id)
+    blogs = Blog.query.all()
+    if 'username' in session:
+        return render_template('individualuser.html', title=user.username, blogs=blogs, id=users_id)
 
 
 def single_blog():
     blog_id = int(request.args.get('id'))
     blog = Blog.query.get(blog_id)
-    return render_template('singleblog.html', title="Current Blog", blog = blog)
+    if 'username' in session:
+        return render_template('singleblog.html', title="Current Blog", blog=blog, username=session['username'])
 
+    return render_template('singleblog.html', title="Current Blog", blog=blog)
 
-@app.route('/newpost')
-def index():
-    return render_template('newpost.html', title="Add Posts")
-
-@app.route('/addpost', methods=['POST'])
+@app.route('/newpost', methods=['POST', 'GET'])
 def add():
+    if request.method == 'POST':
 
-    title_error = ''
-    body_error = ''
+        error = False
 
-    blog_title = request.form['blog']
-    blog_body = request.form['blog_body']
+        blog_title = request.form['blog']
+        blog_body = request.form['blog_body']
+        owner = User.query.filter_by(username=session['username']).first()
 
-    if blog_title == "":
-        title_error = "Please enter a title for you blog"
+        if blog_title == "":
+            flash("Please enter a title for you blog")
+            error = True
 
-    if blog_body == "":
-        body_error = "Please enter content for your blog"
+        elif blog_body == "":
+            flash("Please enter content for your blog")
+            error = True
 
-    if body_error or title_error != '':
-        return render_template('newpost.html', title="Add Posts", title_error=title_error, body_error=body_error)
+        if error == True:
+            return render_template('newpost.html', title="Add Posts")
 
-    new_blog = Blog(blog_title, blog_body)
-    db.session.add(new_blog)
-    db.session.commit()
+        new_blog = Blog(blog_title, blog_body, owner)
+        db.session.add(new_blog)
+        db.session.commit()
 
-    return redirect('/blog')
+        return redirect('/blog')
+
+    return render_template('newpost.html', title="New Posts", username=session['username'])
 
 
 if __name__ == '__main__':
